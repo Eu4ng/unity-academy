4dabee824af1f4de76eb7d0c0215d7d7fac4d8d0
Eu4ng <dnjsvltm911@gmail.com>
2023-05-12 18:08:59 +0900

    Witcher 업데이트 # 프로젝트 세팅 - DeadZone 태그 추가
    # 스크립트 수정
    - Combat2D
      - Friend / Enemy 태그 확인 기능 추가
    
    # 프리팹 수정
    - Player, DeadZone
      - 태그 수정
    - Actor
      - 기본 deathEffect 설정 (Pong)

--- Witcher/Assets/Scripts/Combat2D.cs
+++ Witcher/Assets/Scripts/Combat2D.cs
@@ -6,6 +6,9 @@ public class Combat2D : MonoBehaviour
 {
     // 기본 설정값
     public Stats stats;
+    public List<string> enemyTagNames;
+    public List<string> friendTagNames;
+    public GameObject hitEffect;
 
     // 컴포넌트
     Collider2D actorCollider;
@@ -17,6 +20,44 @@ public class Combat2D : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AttributeSet.GiveDamage(collision.gameObject, stats.GetAttackDamage());
+        // Friend Tag List에 등록된 GameObject들은 무시
+        if (CheckFriend(collision))
+            return;
+
+        // Enemy Tag List에 등록된 GameObject들에는 데미지를 가함
+        // Enemy Tag List가 비어있다면 Friend를 제외한 모든 GameObject들에 데미지 가함
+        if (enemyTagNames.Count <= 0 || CheckEnemy(collision))
+        {
+            SpawnHitEffect();
+            AttributeSet.GiveDamage(collision.gameObject, stats.GetAttackDamage());
+            Destroy(gameObject);
+        }
+    }
+
+    void SpawnHitEffect()
+    {
+        if (hitEffect)
+            Instantiate(hitEffect, transform.position, Quaternion.Euler(Vector2.zero));
+    }
+
+    bool CheckFriend(Collider2D collision)
+    {
+        return CheckTagList(collision, friendTagNames);
+    }
+
+    bool CheckEnemy(Collider2D collision)
+    {
+        return CheckTagList(collision, enemyTagNames);
+    }
+
+    bool CheckTagList(Collider2D collision, List<string> tagList)
+    {
+        foreach (string tag in tagList)
+        {
+            if (collision.gameObject.CompareTag(tag))
+                return true;
+        }
+
+        return false;
     }
 }
