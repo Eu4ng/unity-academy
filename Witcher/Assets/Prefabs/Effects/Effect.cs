4f9d121fd519e4a87b4b469c00e3868495ab777d
Eu4ng <dnjsvltm911@gmail.com>
2023-05-15 16:29:15 +0900

    Effect 프리팹 수정 Effect 프리팹의 Effect 스크립트에 Sprite 이미지와 Animation Clip만 설정함으로써 새로운 이펙트 생성 가능

--- Witcher/Assets/Prefabs/Effects/Effect.cs
+++ Witcher/Assets/Prefabs/Effects/Effect.cs
@@ -4,7 +4,52 @@ using UnityEngine;
 
 public class Effect : MonoBehaviour
 {
-    void SelfDestroy()
+    [SerializeField] Sprite m_Sprite;
+    [SerializeField] AnimationClip m_AnimationClip;
+    Animator m_Animator;
+    int m_AnimationCount = 1;
+
+    private void Awake()
+    {
+        // Sprite Renderer 초기화
+        var spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = m_Sprite;
+
+        // Animator 초기화
+        m_Animator = GetComponent<Animator>();
+
+        SetEffectAnimation();
+        AddAnimationEndEvent();
+    }
+
+    void SetEffectAnimation()
+    {
+        AnimatorOverrideController aoc = new AnimatorOverrideController(m_Animator.runtimeAnimatorController);
+        var anims = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+        if(aoc.animationClips.Length == m_AnimationCount)
+        {
+            anims.Add(new KeyValuePair<AnimationClip, AnimationClip>(aoc.animationClips[0], m_AnimationClip));
+        }
+        aoc.ApplyOverrides(anims);
+        m_Animator.runtimeAnimatorController = aoc;
+    }
+
+    void AddAnimationEndEvent()
+    {
+        int layerIndex = 0;
+        AnimatorClipInfo[] animatorClipInfos = m_Animator.GetCurrentAnimatorClipInfo(layerIndex);
+        if (animatorClipInfos.Length == m_AnimationCount)
+        {
+            AnimationClip clip = animatorClipInfos[0].clip;
+            AnimationEvent endEvent = new AnimationEvent();
+            endEvent.time = clip.length;
+            endEvent.functionName = "DestroyEffect";
+
+            clip.AddEvent(endEvent);
+        }
+    }
+
+    void DestroyEffect()
     {
         Destroy(gameObject);
     }
