84a27358b5179cbac76e42b583b5fd62da1fd26f
Eu4ng <dnjsvltm911@gmail.com>
2023-05-16 14:25:50 +0900

    Monster, Player 몸통 박치기 기능 추가 (Combat2D 추가) Combat2D 프리셋 설정 Witcher Hit 애니메이션 설정 (Has Exit Time false)

--- Witcher/Assets/Scripts/Combat2D.cs
+++ Witcher/Assets/Scripts/Combat2D.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Combat2D : MonoBehaviour
 {
     // 기본 설정값
-    public Stats stats;
-    public List<string> enemyTagNames;
-    public List<string> friendTagNames;
-    public GameObject hitEffect;
+    public Stats m_Stats;
+    public List<string> m_EnemyTagNames;
+    public List<string> m_FriendTagNames;
+    public GameObject m_HitEffect;
+    public bool m_DestroyAfterHit = false;
 
     // 컴포넌트
-    Collider2D actorCollider;
+    Collider2D m_Collider;
 
     private void Awake()
     {
-        actorCollider = GetComponent<Collider2D>();
+        m_Collider = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -26,28 +27,29 @@ public class Combat2D : MonoBehaviour
 
         // Enemy Tag List에 등록된 GameObject들에는 데미지를 가함
         // Enemy Tag List가 비어있다면 Friend를 제외한 모든 GameObject들에 데미지 가함
-        if (enemyTagNames.Count <= 0 || CheckEnemy(collision))
+        if (m_EnemyTagNames.Count <= 0 || CheckEnemy(collision))
         {
             SpawnHitEffect();
-            AttributeSet.GiveDamage(collision.gameObject, stats.GetAttackDamage());
-            Destroy(gameObject);
+            AttributeSet.GiveDamage(collision.gameObject, m_Stats.GetAttackDamage());
+            if(m_DestroyAfterHit)
+                Destroy(gameObject);
         }
     }
 
     void SpawnHitEffect()
     {
-        if (hitEffect)
-            Instantiate(hitEffect, transform.position, Quaternion.Euler(Vector2.zero));
+        if (m_HitEffect)
+            Instantiate(m_HitEffect, transform.position, Quaternion.Euler(Vector2.zero));
     }
 
     bool CheckFriend(Collider2D collision)
     {
-        return CheckTagList(collision, friendTagNames);
+        return CheckTagList(collision, m_FriendTagNames);
     }
 
     bool CheckEnemy(Collider2D collision)
     {
-        return CheckTagList(collision, enemyTagNames);
+        return CheckTagList(collision, m_EnemyTagNames);
     }
 
     bool CheckTagList(Collider2D collision, List<string> tagList)
